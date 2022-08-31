2e6e099644682b10d42f17ab2bf479106b5239aa
GuangjiHuang <976824193@qq.com>
2022-08-31 22:54:42 +0800

    update->2022-08-31 周三 22:54:42

--- coding/python/DL/main.py
+++ coding/python/DL/main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+sentence = input()
+words = input()
+# get the new sentence
+#
+words = words.lower()
+word_ls = words.split(" ")
+sentence_ls = sentence.split(" ")
+word_dict = dict()
+for i, word in enumerate(word_ls):
+    word_dict[word] = str(i)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+ret_ls = list()
+for sen in sentence_ls:
+    if sen.lower() in word_dict.keys():
+        sentence = sentence.replace(sen, word_dict[sen.lower()])
+#
+quote_num = 0
+new_sentence = ""
+for i in range(len(sentence)):
+    if sentence[i] == '"':
+        quote_num += 1
+    if quote_num%2 and sentence[i] in word_dict.values():
+        new_sentence += word_ls[int(sentence[i])]
+    else:
+        new_sentence += sentence[i]
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(new_sentence)
