fe4d3e3447fbf53dc2cae04f03ec7ffc720185e7
GuangjiHuang <976824193@qq.com>
2022-09-09 21:14:20 +0800

    update->2022-09-09 周五 21:14:20

--- coding/python/DL/main.py
+++ coding/python/DL/main.py
@@ -1,17 +1,11 @@
-import re
-#str = "输入_test@qq.com测试ha-ha.wang@SINA.COM.CN"
-#str = "输入@qq.com"
-str = "测试样例hello-world@qq.com-cn"
-#str = input()
-pattern = re.compile(r'([a-zA-Z0-9_][a-zA-Z0-9_.-]{1,}@[0-9a-zA-Z]*(\.[0-9a-zA-Z]{2,}){1,})')
-ret_ls = pattern.findall(str)
+import torch
+from torch import Module
 
-ans = list()
-for ret in ret_ls:
-    ans.append(ret[0])
-if len(ans) == 0:
-    print("false")
-else:
-    ret_str = "true " + " ".join(ans)
-    print(ret_str)
+class ResBlock():
 
+    def __init__(self, n):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
